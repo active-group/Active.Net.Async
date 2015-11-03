4763da9b22fa091a849d06798503b49cd0116e77
Andreas Bernauer <andreas.bernauer@active-group.de>
2015-11-03 10:41:01 +0100

    initial

---
+++ Async.fs
@@ -0,0 +1,30 @@
+﻿namespace Active.Net
+
+module Async =
+    open System.Threading
+
+    let asIfAsynchronously (f: unit -> 'A): Async<'A> =
+        async {
+            let original = System.Threading.SynchronizationContext.Current
+            do! Async.SwitchToNewThread()
+            try
+                let v = f ()
+                do! Async.SwitchToContext(original)
+                return v
+            with
+            | e ->
+                do! Async.SwitchToContext(original)
+                return raise e
+        }
+
+    /// invoke a callback after a bunch of milliseconds, return a function that cancels the timer
+    let timer (milliseconds: int) (callback: Async<unit>): unit -> unit =
+        let cts = new CancellationTokenSource()
+        let token = cts.Token
+        let a = async {
+                    do! Async.Sleep milliseconds
+                    do! callback
+                }
+        Async.Start (a, token)
+        cts.Cancel
+
